c9cd76428d1ace63dc6be1bf96bc4ca669327f8f
billy861230 <billy861230@gmail.com>
2018-04-29 17:53:21 +0800

    pca

--- hw4/pca.py
+++ hw4/pca.py
@@ -4,17 +4,21 @@ import sys
 imgs = []
 
 for i in range(415):
-    img = io.imread('Aberdeen' + str(i) + '.jpg')
+    img = io.imread('Aberdeen/' + str(i) + '.jpg')
     img = np.array(img)
-    print(img.shape())
     img = img.flatten()
     imgs.append(img)
 
 imgs = np.array(imgs)
-x = imgs.T
-x_mean = np.mean(x, axis=1)
 
-u, s, v=np.linalg.svd(x - x_mean, full_matrices=False)
+x_mean = np.mean(imgs, axis=0)
+x = imgs.T
+mean = []
+for i in range(415):
+    mean.append(x_mean)
+mean = np.array(mean)
+
+u, s, v=np.linalg.svd(x - mean.T, full_matrices=False)
 
 y = list(sys.argv[1].split('.'))
 y = int(y[0])
@@ -33,5 +37,4 @@ k = np.array(k)
 y = np.dot(k, u[0:4])
 x_mean = x_mean.T
 y = y + x_mean
-
-
+y = y.reshape(600, 600, 3)
